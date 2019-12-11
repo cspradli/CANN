776a78438c2415802a7c863c702c005a92fc01d6
Caleb Spradlin <cspradli@unca.edu>
2019-12-10 21:08:08 -0500

    added functions to go in hopefully

--- nnet_io.h
+++ nnet_io.h
@@ -7,6 +7,20 @@
 #include <unistd.h>
 #include "nnet.h"
 
+typedef struct
+{
+    double** target_in;
+    double** target;
+    int num_input;
+    int num_output;
+    int num_rows
+} data;
+
+double **init_2d(int rows, int columns);
+
+data *get_data(int num_inputs, int num_outputs, int num_rows);
+
+void free_data(data *in);
 
 void nnet_save(cann_double *nnet, char *path);
 
